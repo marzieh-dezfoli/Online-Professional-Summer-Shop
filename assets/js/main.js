1d97d1f056c9e53169663b226775bca4061045f7
Marzieh Lorzade Dezfoli <marziehlorzadedezfoli@gmail.com>
2025-05-31 11:49:59 -0700

    Added responsive layout to the final section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav_menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
 
 /*===== Menu Show =====*/
 /* Validate if constant exists */
+if(navToggle) {
+navToggle.addEventListener('click', () => {
+navMenu.classList.add('show-menu');
+
+});
+
+}
 
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+if(navClose) {
+navClose.addEventListener('click', () => {
+navMenu.classList.remove('show-menu');
 
+});
+
+}
 
 /*===============Image gallery ===============*/
 function  imgGallery() {
@@ -34,13 +50,24 @@ var swiperCategories = new Swiper(".categories__container", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+    350: {
+      slidesPerView:2,
+      spaceBetween: 24,
     },
+
     768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView:4,
+      spaceBetween: 24,
+    },
+
+
+    992: {
+      slidesPerView:4,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 6,
@@ -58,13 +85,13 @@ var swiperProducts = new Swiper(".new__container", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
     768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
     },
     1400: {
       slidesPerView: 4,
